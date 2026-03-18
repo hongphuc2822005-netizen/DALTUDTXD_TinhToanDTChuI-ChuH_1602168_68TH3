40ae2f9a30be9f6ef287e6e0d6839783edbb6e78
hongphuc2822005-netizen <hongphuc2822005@gmail.com>
2026-03-18 20:53:02 +0700

    update

---
+++ DALTUDTXD_TinhToanDTChuIChuH_1602168_68TH3/DALTUDTXD_TinhToanDTChuIChuH_1602168_68TH3/ViewModels/EtabsAPI_FramesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD_TinhToanDTChuIChuH_1602168_68TH3.ViewModels
+{
+    internal class EtabsAPI_FramesViewModel
+    {
+    }
+}
